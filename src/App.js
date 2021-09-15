fb6e3df2f750ecf7884898c2e641a1ad181effc9
Maks-KaNDer <maks2517.k@gmail.com>
2021-09-15 21:06:16 +0300

    React & Firebase Chat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
 import Chat from './components/Chat';
 import SignIn from './components/SignIn';
-import { auth } from './firebase.js'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDpXkVDx-AYlVc-cLj-Q77x5LWpSE9MgRk",
+    authDomain: "kander-project.firebaseapp.com",
+    projectId: "kander-project",
+    storageBucket: "kander-project.appspot.com",
+    messagingSenderId: "463339837717",
+    appId: "1:463339837717:web:bacb4843cb0319dd5ef11c",
+    measurementId: "G-6D0N3ZEQ4S"
+})
+
+const db = firebaseApp.firestore()
+
+const auth = firebase.auth()
+
+export { db, auth } 
 
 function App() {
   const [user] = useAuthState(auth)
